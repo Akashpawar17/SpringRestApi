b5685d209e7f9fe8fa1e69dd27d9c49794b1f874
Toshiba <Toshiba@192.168.0.5>
2020-06-17 10:29:03 +0530

    spring rest api using spring starter project

---
+++ springdatarest/src/main/java/com/capgemini/AlienRepo.java
@@ -0,0 +1,8 @@
+package com.capgemini;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+@RepositoryRestResource(collectionResourceRel="aliens",path="aliens")
+public interface AlienRepo  extends JpaRepository<Alien, Integer>{
+
+}
---
+++ springdatarest/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+
+insert into alien values (101,'Akash','apple');
+insert into alien values (102,'vikash','vivo');
+insert into alien values (103,'Abd','oneplus');
+insert into alien values (104,'sunny','samsung');
+insert into alien values (105,'john','oppo');
